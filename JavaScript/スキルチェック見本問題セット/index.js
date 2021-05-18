76dff04cdc83d8a1e7bcbe89799e904318e6e470
TakushiTokuyama <sokoninoruna@gmail.com>
2021-05-18 22:53:55 +0900

    長テーブルのうなぎ屋 (paizaランク B 相当)

--- JavaScript/スキルチェック見本問題セット/index.js
+++ JavaScript/スキルチェック見本問題セット/index.js
@@ -1,3 +1,4 @@
+// 標準入力
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
 
@@ -8,34 +9,154 @@ var reader = require('readline').createInterface({
 });
 
 reader.on('line', (line) => {
-    lines.push(parseInt(line));
+    var nums = line.split(" ")
+    for (var i = 0; i < nums.length; i++) {
+        lines.push(parseInt(nums[i]));
+    }
 });
 
 reader.on('close', () => {
-    returnFizzBuzz(lines[0]);
+    
 });
 
+// Excute01();
+
+// Excute02();
+
+// Excute03();
+
+// Excute04(lines[0], lines[1]);
+
+// Excute05();
+
+// Excute06();
+
 // 掛け算
-// let multiplication = () => lines[0] * lines[1];
+function Excute01() {
+    reader.on('line', (line) => {
+        lines.push(parseInt(line));
+    });
+
+    reader.on('close', () => {
+        console.log(multiplication());
+    });
+}
+
+let multiplication = () => lines[0] * lines[1];
 
 // 足し算
-// let addition = () => lines.map(Number).reduce((a, b) => a + b);
+function Excute02() {
+    reader.on('line', (line) => {
+        lines.push(parseInt(line));
+    });
+
+    reader.on('close', () => {
+        console.log(addition());
+    });
+}
+
+let addition = () => lines.map(Number).reduce((a, b) => a + b);
 
 // 一番小さい値
-// let minimumValue = () => Math.min.apply(null, lines);
+function Excute03() {
+    reader.on('line', (line) => {
+        lines = line.split(" ")
+    });
+
+    reader.on('close', () => {
+        console.log(minimumValue());
+    });
+}
+let minimumValue = () => Math.min.apply(null, lines);
 // let minimumValue = () => lines.reduce((a, b) => (a < b ? a : b)); 
 // let minimumValue = () => lines.sort((a, b) => (a - b)); 
 
 // 文字の一致
-// let isCharacterMatch = (a, b) => a == b ? 'OK' : 'NG';
+function Excute04() {
+    reader.on('line', (line) => {
+        lines = line.split(" ")
+    });
+
+    reader.on('close', () => {
+        console.log(isCharacterMatch(lines[0], lines[1]));
+    });
+}
+
+let isCharacterMatch = (a, b) => a === b ? 'OK' : 'NG';
 
 // Fizz Buzz
-let returnFizzBuzz = n => {
-    for (let i = 1; i <= n; i++) {
-        var result = i % 3 === 0 && i % 5 === 0 ? 'Fizz Buzz'
-                                                : i % 3 === 0 ? 'Fizz'
-                                                : i % 5 === 0 ? 'Buzz'
-                                                : i;
-        console.log(result);
+function Excute05() {
+
+    reader.on('line', (line) => {
+        lines.push(parseInt(line));
+    });
+
+    reader.on('close', () => {
+        returnFizzBuzz(lines[0]);
+    });
+
+    let returnFizzBuzz = n => {
+        for (let i = 1; i <= n; i++) {
+            var result = i % 3 === 0 && i % 5 === 0 ? 'Fizz Buzz'
+                : i % 3 === 0 ? 'Fizz'
+                    : i % 5 === 0 ? 'Buzz'
+                        : i;
+            console.log(result);
+        }
+    };
+}
+
+// 長テーブルのうなぎ屋
+function Excute06() {
+    // 座席
+    var seats = new Array(lines[0]).fill(0);
+
+    // 人数と開始番号
+    var NumberOfPeopleAndStartNumber = lines.splice(2, lines[1] * 2);
+
+    // 人数
+    var peoples = NumberOfPeopleAndStartNumber.filter(function (value, index) {
+        if (index % 2 === 0) {
+            return value;
+        }
+    });
+
+    // 開始番号
+    var startNumbers = NumberOfPeopleAndStartNumber.filter(function (value, index) {
+        if (index % 2 === 1) {
+            return value;
+        }
+    }).map((a) => a - 1);
+
+    // グループ数分座席に座る処理
+    for (var i = 0; i < NumberOfPeopleAndStartNumber.length / 2; i++) {
+        var sitting = false;
+        // 座席の確認
+        for (var x = 0; x < peoples[i]; x++) {
+            if (seats[startNumbers[i]] + x > seats.length) {
+                if (seats[0] === 1) {
+                    sitting = true;
+                    break;
+                }
+            } else {
+                if (seats[startNumbers[i] + x] === 1) {
+                    sitting = true;
+                    break;
+                }
+            }
+        }
+
+        // 座先確定
+        if (!sitting) {
+            for (var y = 0; y < peoples[i]; y++) {
+                if (seats[startNumbers[i]] + y > seats.length) {
+                    seats[0] = 1;
+                } else {
+                    seats[startNumbers[i] + y] = 1;
+                }
+            }
+        }
     }
-};
+    // 座席に座っている人数
+    console.log(seats.reduce((sum, num) => sum + num, 0));
+}
